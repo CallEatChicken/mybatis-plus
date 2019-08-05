46739dc7260b2c9506806454f3528eb1ba6d390e
wzf <yd@szax.com>
2019-08-05 17:11:51 +0800

    1.0

--- src/main/java/com/mybatis/plus/config/annotation/NeedSetValue.java
+++ src/main/java/com/mybatis/plus/config/annotation/NeedSetValue.java
@@ -1,11 +1,9 @@
 package com.mybatis.plus.config.annotation;
 
-import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
 
-@Target(ElementType.FIELD)
+//@Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface NeedSetValue {
 
